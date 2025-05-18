dcb5a5750ccf7bb41810b19a4c23ad918d738aae
Marvin Pahling <marvinpahling@gmail.com>
2025-05-18 13:14:25 +0200

    lsp setup

--- lua/marvin/plugins/lsp/lspconfig.lua
+++ lua/marvin/plugins/lsp/lspconfig.lua
@@ -20,17 +20,20 @@ return {
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 
-				opts.desc = "Show LSP references"
-				vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
 				opts.desc = "Go to declaration"
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP references"
+				vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
 				opts.desc = "Show LSP definitions"
 				vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
 				opts.desc = "Show LSP implementations"
 				vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+				opts.desc = "Show LSP type definitions"
+				vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
 				opts.desc = "Show LSP type definitions"
 				vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
@@ -47,11 +50,11 @@ return {
 				opts.desc = "Show line diagnostics"
 				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-				opts.desc = "Go to previous diagnostic"
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-				opts.desc = "Go to next diagnostic"
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				-- opts.desc = "Go to previous diagnostic"
+				-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				--
+				-- opts.desc = "Go to next diagnostic"
+				-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 				opts.desc = "Show documentation for what is under cursor"
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -94,6 +97,16 @@ return {
 							},
 						},
 					},
+				})
+			end,
+			["rust_analyzer"] = function()
+				lspconfig["rust_analyzer"].setup({
+					capabilities = capabilities,
+				})
+			end,
+			["zls"] = function()
+				lspconfig["zls"].setup({
+					capabilities = capabilities,
 				})
 			end,
 		})
