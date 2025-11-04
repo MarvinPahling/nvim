87515ddcba545005cf000c63045fbf38b116716e
Marvin Pahling <marvinpahling@gmail.com>
2025-11-04 12:33:29 +0100

    add embedded

--- lua/plugins/embedded.lua
+++ lua/plugins/embedded.lua
@@ -1,4 +1,5 @@
 return {
+	{ "krady21/compiler-explorer.nvim" },
 	{
 		"anurag3301/nvim-platformio.lua",
 		cmd = { "Pioinit", "Piorun", "Piocmdh", "Piocmdf", "Piolib", "Piomon", "Piodebug", "Piodb" },
